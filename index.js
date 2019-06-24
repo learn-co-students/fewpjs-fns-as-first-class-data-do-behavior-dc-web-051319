ea8d90f0bdd9db7ea7c23c3229e29d524808d799
Matt Williams <matt.seongjun@gmail.com>
2019-06-24 11:38:44 -0400

    Switch statements are fun

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+    const timeString = document.getElementById('time').value
+    displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string){
+    switch (true) {
+        case string.slice(0,2) > (12 + 5):
+            return 'Good Evening';
+            break;
+        case string.slice(0,2) >= 12 && string.slice(0,2) <= (12 + 5):
+            return 'Good Afternoon'
+            break;
+        case string.slice(0,1) < 12:
+            return 'Good Morning';
+            break;
+    };
+};
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+    let loc = document.getElementById("greeting");
+    loc.innerText = string;
+};
