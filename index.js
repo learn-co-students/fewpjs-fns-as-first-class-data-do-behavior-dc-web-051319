e2101459679546cbb29e0b995eaaf4230d6f09a8
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-24 11:38:28 -0400

    Completed lab

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+let greet = function(time) {
+  let parsedTime = time.split(":")[0]
+  if (parsedTime > 17) {
+    return "Good Evening"
+  } else if (parsedTime < 12) {
+    return "Good Morning"
+  } else {
+    return "Good Afternoon"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+let displayMessage = function(str) {
+  let greeting = document.getElementById('greeting')
+  greeting.innerText = str
+}
