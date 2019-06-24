4d71969f94c9404b6ac4b848eab3bab23c9ca7a2
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-06-24 11:55:50 -0400

    Used an event listener to update text on the DOM

--- index.js
+++ index.js
@@ -8,4 +8,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+timeString = "8:40"
+
+function greet(timeString) {
+  let hour = parseInt(timeString.split(':')[0])
+
+  if (hour < 12) {
+    return "Good Morning"
+  } else if (hour > 12 && hour < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+greet(timeString)
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  const greeting = document.getElementById('greeting')
+  greeting.innerText = `${string}`
+}
