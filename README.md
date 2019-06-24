f305ad1fe30c873aaeafd4a265f63f058e291f36
2daysweb <sahnunhm@gmail.com>
2019-06-24 18:40:31 -0400

    Done.

--- README.md
+++ README.md
@@ -22,10 +22,10 @@ button, some provided code will run which will make calls to **two** functions.
 The `greet` function should take one argument, a `String` which specifies the
 24-hour time in the format `HH:MM`.
 
-- If the time is earlier than 12pm, return "Good Morning".
+<!-- - If the time is earlier than 12pm, return "Good Morning".
 - If the time is between 12pm and 5pm, return "Good Afternoon".
 - If the time is later than 5pm, return "Good Evening".
-
+ -->
 ##  The `displayMessage()` function
 
 The `displayMessage` function should take one argument, a `String`.
@@ -34,12 +34,12 @@ When the function runs it should update the text inside the `#greeting` node
 with the content of the first argument.
 
 It does not return anything.
-
+<!-- 
 **NOTE:** The value returned from the `<input>` will be of type `String`.
 You’ll need to take the `String` of the 24 hour time and covert it to a number.
 The `split()` [function](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split)
 and the `parseInt()` [functions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt) should help.
-
+ -->
 ## Conclusion
 
 This lab demonstrates just the tip of the iceberg when it comes to leveraging
--- index.js
+++ index.js
@@ -5,7 +5,57 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+
+
+
+// <!-- 
+// **NOTE:** The value returned from the `<input>` will be of type `String`.
+// You’ll need to take the `String` of the 24 hour time and covert it to a number.
+// The `split()` [function](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split)
+// and the `parseInt()` [functions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt) should help.
+//  -->
+
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+
+
+// <!-- - If the time is earlier than 12pm, return "Good Morning".
+// - If the time is between 12pm and 5pm, return "Good Afternoon".
+// - If the time is later than 5pm, return "Good Evening".
+//  -->
+
+
+
+
+function greet(string) {
+
+	let hour = string.split(":")
+
+	let hours = parseInt(hour[0]) 
+
+	console.log(hours)
+
+
+	if(hours < 12) {
+		return console.log("Good Morning")
+	}
+
+	if (12 < hours && hours < 17) {
+
+		return console.log("Good Afternoon")
+	}
+
+	if (17 < hours && hours < 24) {
+
+		return console.log("Good Evening")
+	}
+
+}
+
+
+function displayMessage(string){
+
+	console.log("Displaying")
+
+}
