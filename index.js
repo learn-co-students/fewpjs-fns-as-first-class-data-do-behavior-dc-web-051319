48fdab3699c730764708369e44142b27d7fd01f7
github username <github email address>
2019-06-24 13:37:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+// import { generateKeyPair } from "crypto";
+
 /* Given Code, don't edit */
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
-}
+};
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string){
+  let time = parseInt(string) 
+  if (time < 12){ return "Good Morning" }
+  if(time > 12 && time < 17){ return "Good Afternoon" }
+  return "Good Evening"
+};
+
 /* Write your implementation of displayMessage() */
+ function displayMessage(string){
+   document.getElementById("greeting").innerText = string
+};
+
